2ff41f842b8c43bdc15df686140e875ca0c272f7
nakamurayuto12 <73273993+nakamurayuto12@users.noreply.github.com>
2020-11-20 17:50:02 +0900

    編集、削除ボタンの表示

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -17,8 +17,15 @@ class PrototypesController < ApplicationController
   end
 
   def show
+    @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+  
   private
 
   def prototype_params
